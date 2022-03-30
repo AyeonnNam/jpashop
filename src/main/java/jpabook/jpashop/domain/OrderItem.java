f0d6cb2d92df1d318c3d8e1cd3346d2963657c3c
name <boalove284@naver.com>
2022-03-30 12:03:41 +0900

    entity 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
+@Table(name="order_item")
 @Getter
 @Setter
 public class OrderItem {
@@ -16,7 +17,7 @@ public class OrderItem {
     private Long id;
 
     @ManyToOne
-    @Column(name="order_id")
+    @JoinColumn(name="order_id")
     private Order order;
 
     @ManyToOne
